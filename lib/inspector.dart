6940e8aca174ffcc19aa8906654cf919176a552a
A <amit.jha6700@gmail.com>
2022-05-28 01:28:16 +0530

    initial commit

---
+++ lib/inspector.dart
@@ -0,0 +1,14 @@
+library inspector;
+
+import 'package:flutter/material.dart';
+
+class InspectorApp extends StatelessWidget {
+  const InspectorApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Inspecor')),
+    );
+  }
+}
